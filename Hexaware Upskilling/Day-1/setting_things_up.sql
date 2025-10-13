86f6c102277c03d7cc275f864373f6cbad43d997
Tharun Atithya <tharunaadhi6@gmail.com>
2025-10-13 11:40:36 +0530

    Add initial Snowflake setup with warehouse, database, and schema for Day-1 training

---
+++ Hexaware Upskilling/Day-1/setting_things_up.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE WAREHOUSE tharun WITH 
+    WAREHOUSE_TYPE = 'SNOWPARK-OPTIMIZED'
+    WAREHOUSE_SIZE = 'MEDIUM'
+    AUTO_SUSPEND = 300
+    AUTO_RESUME = TRUE
+    COMMENT = 'Training warehouse just to practice.'
+
+CREATE OR REPLACE DATABASE tharun_learning
+
+CREATE OR REPLACE SCHEMA day_1
+    COMMENT = 'All day-1 learning will be executed from this schema'
+
+USE WAREHOUSE tharun
+
